d4027c08701f2afb549f3c39f04bb7dc95d58e98
fortune chibuike <chibuikefortune@hotmail.com>
2017-07-19 18:02:16 +0100

    xgf

--- app/controllers/balances_controller.rb
+++ app/controllers/balances_controller.rb
@@ -1,12 +1,17 @@
 class BalancesController < InheritedResources::Base
 
-  before_action :find_Balance, only: [ :show, :edit, :update, :destroy]
+  before_action :find_Balance, only: [  :edit, :update, :destroy]
   before_action :authenticate_user!
   def index
     @balances = Balance.where(user_id: current_user)
 
   end
   def show
+    if valid_page?
+   render template: "balances/#{params[:id]}"
+ else
+   render file: "public/404.html", status: :not_found
+ end
   end
 
   def new
@@ -39,10 +44,16 @@ end
     @balance.destroy
   end
 
+
+
+
   private
   def find_Balance
     @balance = Balance.find(params[:id])
   end
+  def valid_page?
+     File.exist?(Pathname.new(Rails.root + "app/views/balances/#{params[:id]}.html.erb"))
+   end
     def balance_params
       params.require(:balance).permit(:balance)
     end
