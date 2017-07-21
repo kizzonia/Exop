062df39586ffc145e0f8e33c61cbb28a219cfc82
fortune chibuike <chibuikefortune@hotmail.com>
2017-07-21 19:54:53 +0100

    push

--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -1,9 +1,5 @@
 if Rails.env.production?
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.perform_deliveries = true
- config.action_mailer.default :charset => "utf-8"
-
-  config.action_mailer.delivery_method = :smtp
+ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
